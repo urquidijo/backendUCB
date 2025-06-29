3be0411bb7d121a4ae307e713caf03f3b51289fa
juliocesarbal <juliocesarbaldiviezomoron@gmail.com>
2025-06-28 22:33:28 -0400

    cambios

--- src/routes/bitacora.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registrarEntrada,getBitacora } from "../controllers/bitacora.controller.js";
-
-const router = express.Router();
-
-router.get('/',getBitacora); 
-router.post('/entrada', registrarEntrada);
-
-export default router;
--- src/routes/historialVentas.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getHistorialVentas, deleteVenta  } from "../controllers/historialVentas.controller.js";
-
-const router = Router();
-
-// Ruta para obtener historial de ventas
-router.get("/", getHistorialVentas);
-router.delete("/:id", deleteVenta);
-export default router;
